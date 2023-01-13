1cd89635b03283731558a070b1f4daf319475a33
Charles Swaleh <charlswall@live.com>
2023-01-13 15:44:53 +0300

    Completed javascript code to show an operator's details in the main page

---
+++ src/index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetchOperators()
+})
+
+const localDBURL = "http://localhost:3000/operators"
+
+function fetchOperators(){
+  fetch(`${localDBURL}`)
+  .then(res => res.json())
+  .then(data => {
+    displayOperator.call(data[12])
+  })
+}
+
+function displayOperator(){
+  document.querySelector(".branch-image").alt = this.branch
+  document.querySelector(".branch-image").src = `./assets/${this.branch.toLowerCase()}.png`
+  document.getElementById("operator-name").textContent = `${this.surname}, ${this.name}`
+  document.getElementById("operator-rank").textContent = this.rank
+  document.getElementById("operator-team").textContent = this.team
+}
